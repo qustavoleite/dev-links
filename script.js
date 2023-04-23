b136db1d2a94cbd1dda5558a53ec9a371345b912
qustavoleite <qustavoleite@gmail.com>
2023-04-23 12:31:31 -0300

    add readme

--- script.js
+++ script.js
@@ -2,6 +2,13 @@ function toggleMode() {
   const html = document.documentElement
   html.classList.toggle("light")
 
+  /* if (html.classList.contains("light")) {
+       html.classList.remove("light");
+     } else {
+       html.classList.add("light");
+     }
+   */
+
   /* // pegar a tag img
   const img = document.querySelector("#profile img")
 
